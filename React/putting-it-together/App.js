f79d68bed02a4d03f872bb702c0dde092ff7421a
Ian Hwang <44934582+meen813@users.noreply.github.com>
2022-07-07 17:03:39 -0700

    Add files via upload

---
+++ React/putting-it-together/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonCard from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard firstName = {"Jane"} lastName = {"Doe"} age={45} hairColor={"Black"}></PersonCard>
+      <PersonCard firstName = {"John"} lastName = {"Smith"} age={88} hairColor={"Brown"}></PersonCard>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/putting-it-together/components/PersonCard.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+    
+class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+
+    render(){        
+        return(
+            <div className="item">
+                <h3>{this.props.lastName}, {this.props.firstName}</h3>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {this.props.hairColor}</p>
+
+                <button onClick={()=>this.setState({age: this.state.age+1}) }>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
+
+            </div>
+        )
+    }
+}
+
+export default PersonCard;
